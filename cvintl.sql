895fe7be1fa88fccc92602b324e7e56c45c18da4
qtpeters <quinn.t.peters@gmail.com>
2018-12-08 06:38:11 -0500

    Spreadsheet imports into DB.  Not sure about data integrity

--- cvintl.sql
+++ cvintl.sql
@@ -20,12 +20,15 @@ CREATE TABLE IF NOT EXISTS Giving_Category (
 
 CREATE TABLE IF NOT EXISTS FGrant (
   amount INTEGER,
+  year INTEGER,
   description TEXT,
   contact TEXT,
   telephone TEXT,
   foundation_id LONG,
   recipient_id INTEGER,
+  giving_cat_id, INTEGER,
   FOREIGN KEY( foundation_id ) REFERENCES Foundation( ein ),
   FOREIGN KEY( recipient_id ) REFERENCES Recipient( id )
+  FOREIGN KEY( giving_cat_id ) REFERENCES Giving_Category( id )
 );
 
--- mkdb.py
+++ mkdb.py
@@ -2,38 +2,76 @@
 import xlrd
 import sqlite3
 
-workbook = xlrd.open_workbook( "Adam-Spreadsheet.xlsx" )
-sheet = workbook.sheet_by_name( u"Conservation + Int'l Data" ) 
-conn = sqlite3.connect( 'cvintl.db' )
-c = conn.cursor()
+ein_set = set([])
+rname_set = set([])
+gc_name = set([])
 
-itr = sheet.get_rows()
-next( itr )
-
-einset = set([])
-
-for row in itr:
-
-  ein = row[0].value
-  if ( not ein ): 
-    break
-
-  if ( ein in einset ):
-    continue
-
-  einset.add( ein )
-
-  name = row[1].value
-  city = row[2].value
-  state = row[3].value
+def insert_foundation( ein, name, city, state, c ):
   
-  query = 'INSERT INTO Foundation ( ein, name, city, state )'
-  query = query + ' values ( %d, "%s", "%s", "%s" );' % ( int( ein ), 
-                                                          name, 
-                                                          city, 
-                                                          state )
-  c.execute( query )
-  print( query )
+  if ( not ( ein in ein_set ) ):
+    ein_set.add( ein )
+    query = 'INSERT INTO Foundation ( ein, name, city, state )'
+    query = query + ' values ( %d, "%s", "%s", "%s" );' % ( int( ein ), 
+                                                          name, city, state )
+    c.execute( query )
 
-conn.commit()
-conn.close()
+  c.execute( "SELECT last_insert_rowid();" )
+  return c.fetchone()[0]
+
+def insert_recipient( name, city, state, c ):
+  query = 'INSERT INTO Recipient ( name, city, state )'
+  query = query + ' values ( "%s", "%s", "%s" );' % ( name, 
+                                                      city, 
+                                                      state )
+  c.execute( query )
+  c.execute( "SELECT last_insert_rowid();" )
+  return c.fetchone()[0]
+
+def insert_giving_category( name, c ):
+  query = 'INSERT INTO Giving_Category ( name )'
+  query = query + ' values ( "%s" );' % name 
+  c.execute( query )
+  c.execute( "SELECT last_insert_rowid();" )
+  return c.fetchone()[0]
+
+def insert_grant( amt, year, desc, contact, tele, ein, rid, gc_id, c ):
+
+  print( "Amt: %s" % amt )
+  query = 'INSERT INTO FGrant ( amount, year, description, '
+  query = query + 'contact, telephone, foundation_id, recipient_id, giving_cat_id  )'
+  query = query + ' values ( %d, %d, "%s", "%s", "%s", %d, %d, %d );' % ( int( amt ), int( year ), desc, 
+                                                      contact, tele, int( ein ), 
+                                                      int( rid ), int( gc_id ) )
+  c.execute( query )
+  c.execute( "SELECT last_insert_rowid();" )
+  return c.fetchone()[0]
+
+def main():
+  workbook = xlrd.open_workbook( "Adam-Spreadsheet.xlsx" )
+  sheet = workbook.sheet_by_name( u"Conservation + Int'l Data" ) 
+  conn = sqlite3.connect( 'cvintl.db' )
+  c = conn.cursor()
+
+  itr = sheet.get_rows()
+  next( itr )
+
+  for row in itr:
+
+    if ( not row[0].value ): 
+      continue
+
+    ein = insert_foundation( row[0].value, row[1].value, row[2].value, row[3].value, c )
+    r_id = insert_recipient( row[4].value, row[5].value, row[6].value, c )
+    gc_id = insert_giving_category( row[7].value, c )
+    insert_grant( 
+      row[8].value, 
+      row[9].value, 
+      row[10].value, 
+      row[11].value, 
+      row[12].value,
+      ein, r_id, gc_id, c )
+
+  conn.commit()
+  conn.close()
+
+main()
